1e4579aff4e17ec3285ef947b1a7cdccd0047c87
Zaer Bhuiyan <62622911+zaer04@users.noreply.github.com>
2025-09-24 12:33:34 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("#daySelector").submit(function(event) {
-        event.preventDefault();
-        var formData = $(this).serialize();
-        $.post("scheduler.php", formData, function(data) {
-            $("#activities").html(data);
-        });
-    });
-});
